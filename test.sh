d663273556bda0c0c4a95aa9030ba7a9a785bfc5
camachoo18 <estivencamachops@gmail.com>
2025-02-06 12:43:28 +0100

    Añado sh para deploys

---
+++ test.sh
@@ -0,0 +1,2 @@
+curl -X POST -H "Content-Type: application/json" -H "apikey: 123456" -d '{"message": "Un mensaje"}' http://dev3.cyberbunny.online:3000/message
+curl -H "apikey: 123456" http://dev3.cyberbunny.online:3000/message
