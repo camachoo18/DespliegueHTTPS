d2e711c3759200570e637e3fa49fd375d3c1cfaa
camachoo18 <estivencamachops@gmail.com>
2025-02-13 11:57:45 +0100

    fix

--- prueba.js
+++ prueba.js
@@ -2,14 +2,18 @@ const express = require('express');
 const cors = require('cors');  // Importamos el paquete cors
 const app = express();
 
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
 // Usamos CORS para permitir solicitudes desde cualquier origen
 app.use(cors()); 
 
 // Ruta principal que responde con el objeto
-app.get('/', (req, res) => {
+app.get('/message', (req, res) => {
   res.json({ mensaje: "hola soy camacho" });
 });
-
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'neocities.html'));
+  });
 // Establece el puerto en el que el servidor estará escuchando
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
